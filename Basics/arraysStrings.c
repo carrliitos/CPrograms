550ee402ae7a4329a239aceef0648dd3495a2fff
carrliitos <salazar.ten80@gmail.com>
2020-07-03 09:34:00 -0500

    i faced some errors with the arraysStrings.c

---
+++ Basics/arraysStrings.c
@@ -0,0 +1,11 @@
+/* Arrays: Declare and allocate space for array A */
+#include <stdio.h>
+
+int A[10];
+for (int i = 0; i < 10; i++) {
+	A[i] = 0;
+}
+
+/* Strings: arrays of char terminated by \0 */
+char[] name = "carlitos";
+name[4] = "5";
