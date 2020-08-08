6b3e00be3c61e7996aa5c0d9d458a655704761a6
carrliitos <salazar.ten80@gmail.com>
2020-08-07 21:04:18 -0500

    hello world!

---
+++ TutorialIntroduction/FahrTable.c
@@ -0,0 +1,4 @@
+/* The following program uses the formula °C = (5/9)(°F-32) to print the following
+	table of Fahrenheit temperatures and their centigrade or Celsiu equivalent:
+	
+*/
--- TutorialIntroduction/hello.c
+++ TutorialIntroduction/hello.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> // tells the compiler to include information about the standard I/O library
 
-main() {
-	printf("hello, world!\n");
+
+main() { // define a function called main, similar to JAVA, with no parameters
+	printf("hello, world!\n"); // calls the printf() function
+	printf("Hello, ");
+	printf("world! take 2");
+	printf("\n");
 }
