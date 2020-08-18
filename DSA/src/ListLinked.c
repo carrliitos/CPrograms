6dd0d68804cae668678629cdf9d0932428092b3b
carrliitos <salazar.ten80@gmail.com>
2020-08-18 14:19:39 -0500

    updated notes on linked lists

--- DSA/src/ListLinked.c
+++ DSA/src/ListLinked.c
@@ -5,3 +5,13 @@ typedef struct list {
 	int item; // Data item
 	struct list *next; // point to the successor
 } list;
+
+list *search_list(list *l, int x) {
+	// check if the list is empty
+	if(l == NULL) return(NULL);
+	// if the item on the list == x (what we're looking for), return the pointer
+	if(l -> item == x)
+		return(l);
+	else // recursively search the next item
+		return(search_list(l -> next, x));
+}
