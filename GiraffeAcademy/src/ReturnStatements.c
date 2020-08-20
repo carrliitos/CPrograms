ef01a0cb61689a416c9fc456ffd2c0114b7b34b0
carrliitos <salazar.ten80@gmail.com>
2020-08-20 13:00:44 -0500

    return statements

--- GiraffeAcademy/src/ReturnStatements.c
+++ GiraffeAcademy/src/ReturnStatements.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Function prototype
+	return_type function_name( argument list )
+*/
+
 double cube(double num) {
 	double result = num * num * num;
 	return result;
@@ -13,3 +17,12 @@ int main() {
 	printf("The cube of %d is %.2f", 3, cube(3.2));
 	return 0;
 }
+
+/* The structure of our functions are important.
+	- We need to create our function first before we call them.
+	- If we want to keep our main() above, we would need to prototype the created
+	functions needed for the main() method to run
+	- protoype syntax:
+		return_type function_name( argument list )
+	- And these needs to be declared at the top level
+*/
