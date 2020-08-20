1c4780d73e46bae36384a566b463e92e5db6e648
carrliitos <salazar.ten80@gmail.com>
2020-08-20 01:04:16 -0500

    updated

--- Backlog/TutorialIntroduction/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // tells the compiler to include information about the standard I/O library
-
-
-main() { // define a function called main, similar to JAVA, with no parameters
-	printf("hello, world!\n"); // calls the printf() function
-	printf("Hello, ");
-	printf("world! take 2");
-	printf("\n");
-}
