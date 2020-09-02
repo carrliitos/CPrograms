280f84a7d6e167e1a9bede8cb4c20817a6bfa273
carrliitos <salazar.ten80@gmail.com>
2020-09-02 12:29:49 -0500

    symbolic constants

--- GiraffeAcademy/src/Constants.c
+++ GiraffeAcademy/src/Constants.c
@@ -2,10 +2,26 @@
 #include <stdlib.h>
 
 // Constants = values that are unmodifiable
+/* They can also be defined with symbolic constants 
+	A #define line defines a symbolic name or symbolic constant to be a particular string of characters
+	Syntax:
+		#define name replacement_text
+*/
+
+#define HELLO "hello"
+#define WORLD "world"
+#define THIS "This"
+#define IS "is"
+#define USING "using"
+#define SYMBOLIC "symbolic"
+#define CONSTANTS "constants"
 
 int main() {
 	const int num = 5;
 	printf("%d is a constant.\n", num);
+
+	printf("%s, %s!\n", HELLO, WORLD);
+	printf("%s %s %s %s %s!\n", THIS, IS, USING, SYMBOLIC, CONSTANTS);
 
 	return 0;
 }
