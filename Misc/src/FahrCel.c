f700b829c0807cde7a7d4f8f39d1894415c62a57
carrliitos <salazar.ten80@gmail.com>
2020-09-02 12:23:35 -0500

    misc

---
+++ Misc/src/FahrCel.c
@@ -0,0 +1,10 @@
+/* This program writes the conversion between fahrenheit and celsius table */
+
+#include <stdio.h>
+
+int main() {
+	int fahr;
+
+	for(fahr = 0; fahr <= 300; fahr = fahr + 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * fahr - 32);
+}
